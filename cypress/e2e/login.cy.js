0326655e2caae2ef2d1416453627486fdf0dcb34
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2022-10-23 10:13:40 +0100

    Invalid Username

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,7 +3,7 @@ describe('Login Validation Tests', () => {
 
     it('Valid Login Test', () => {
         cy.visit('https://sridharbandi.github.io/forms/')
-        cy.get('#email').type('test@test.com')
+        cy.get('#email').type('test@test.co')
         cy.get('#password').type('Test@123')
         cy.contains('Login').click()
         cy.contains('Logout').should('be.visible')
